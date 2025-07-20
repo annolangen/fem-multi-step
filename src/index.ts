85dbea5716a1edb17b58b40ee17fdaeaf1ad5ae1
Anno Langen <anno.langen@gmail.com>
2025-07-19 19:13:47 -0700

    Some cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { render, html, svg, TemplateResult } from "lit-html";
-import { classMap } from "lit-html/directives/class-map.js";
+import { render, html, TemplateResult } from "lit-html";
 
 const arcadeIcon = html`<svg
   xmlns="http://www.w3.org/2000/svg"
@@ -68,8 +67,6 @@ const thankYouIcon = html`<svg
   </g>
 </svg>`;
 
-const stepTags = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
-
 type Schedule = "monthly" | "yearly";
 type Plan = {
   [key in Schedule]: number;
@@ -93,6 +90,8 @@ interface State {
   plan: Plan;
   addons: Set<Addon>;
 }
+
+const stepTags = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
 
 const plans: Plan[] = (() => {
   const plan = (name: string, icon: TemplateResult, monthly: number): Plan => ({
@@ -148,9 +147,10 @@ function labeledInputHtml(label: string, placeholder: string, key: string) {
   function onchange(this: HTMLInputElement) {
     state[key] = this.value;
   }
+  const inputId = `input-${key}`;
   return html`
     <div class="flex flex-row justify-between">
-      <span class="mx-4 mt-4 font-semibold text-blue-950">${label}</span>
+      <label for=${inputId} class="mx-4 mt-4 font-semibold text-blue-950">${label}</label>
       ${
         state.validate_step != 1 || !!state[key]
           ? null
@@ -160,6 +160,7 @@ function labeledInputHtml(label: string, placeholder: string, key: string) {
       }
       </div>
       <input
+        id=${inputId}
         class="mx-4 border rounded-sm p-2"
         type="text"
         placeholder=${placeholder}
@@ -171,7 +172,7 @@ function labeledInputHtml(label: string, placeholder: string, key: string) {
 }
 
 const step1CardHtml = () =>
-  html` <h1 class="text-3xl mx-4 mt-4 font-bold text-blue-950">
+  html` <h1 class="mx-4 mt-4 text-3xl font-bold text-blue-950">
       Personal info
     </h1>
     <p class="mx-4 mt-4 text-xl text-gray-500">
@@ -191,12 +192,9 @@ const planHtml = (plan: Plan) =>
       @click=${() => (state.plan = plan)}
     />
     <div
-      class="mx-4 my-2 md:my-4 border rounded-lg flex flex-row md:flex-col border-grey-500 hover:border-purple-600 ${classMap(
-        {
-          "border-purple-600": state.plan === plan,
-          "bg-blue-100": state.plan === plan,
-        }
-      )}"
+      class="border-grey-500 ${state.plan === plan
+        ? "border-purple-600 bg-blue-100"
+        : ""} mx-4 my-2 flex flex-row rounded-lg border hover:border-purple-600 md:my-4 md:flex-col"
     >
       <div class="m-4">${plan.icon}</div>
       <div class="flex flex-grow flex-col">
@@ -211,14 +209,12 @@ const planHtml = (plan: Plan) =>
 
 const billingToggle = () =>
   html` <label
-    class="mx-4 my-2 py-4 bg-blue-100 rounded-lg flex flex-row justify-center items-center cursor-pointer"
+    class="mx-4 my-2 flex cursor-pointer flex-row items-center justify-center rounded-lg bg-blue-100 py-4"
   >
     <p
-      class="mx-4 ${classMap({
-        "text-blue-950": monthlyBilling(),
-        "font-semibold": monthlyBilling(),
-        "text-gray-500": !monthlyBilling(),
-      })}"
+      class="${monthlyBilling()
+        ? "text-blue-950 font-semibold"
+        : "text-gray-500"}"
     >
       Monthly
     </p>
@@ -230,18 +226,16 @@ const billingToggle = () =>
       @click=${() => (state.schedule = monthlyBilling() ? "yearly" : "monthly")}
     />
     <div
-      class="flex items-center w-7 h-4 bg-blue-950 rounded-full mx-4 ${classMap(
-        { "justify-end": !monthlyBilling() }
-      )}"
+      class="${monthlyBilling()
+        ? ""
+        : "justify-end"} mx-4 flex h-4 w-7 items-center rounded-full bg-blue-950"
     >
-      <span class="w-3 h-3 bg-white rounded-full"></span>
+      <span class="h-3 w-3 rounded-full bg-white"></span>
     </div>
     <p
-      class="mx-4 ${classMap({
-        "text-blue-950": !monthlyBilling(),
-        "font-semibold": !monthlyBilling(),
-        "text-gray-500": monthlyBilling(),
-      })}"
+      class=${monthlyBilling()
+        ? "text-gray-500"
+        : "text-blue-950 font-semibold"}
     >
       Yearly
     </p>
@@ -249,7 +243,7 @@ const billingToggle = () =>
 
 const step2CardHtml = () =>
   html`<div class="flex flex-col">
-    <h1 class="text-3xl mx-4 mt-4 font-bold text-blue-950">Select your plan</h1>
+    <h1 class="mx-4 mt-4 text-3xl font-bold text-blue-950">Select your plan</h1>
     <p class="mx-4 mt-4 text-gray-500">
       You have the option of monthly or yearly billing.
     </p>
@@ -263,16 +257,13 @@ const addonHtml = (addon: Addon) => {
   const isSelected = state.addons.has(addon);
   return html`
     <label
-      class="cursor-pointer flex flex-row items-center border border-purple-200 rounded-lg p-4 my-2 hover:border-purple-600 ${classMap(
-        {
-          "border-purple-600": isSelected,
-          "bg-blue-100": isSelected,
-        }
-      )}"
+      class="${isSelected
+        ? "border-purple-600 bg-blue-100"
+        : ""} my-2 flex cursor-pointer flex-row items-center rounded-lg border border-purple-200 p-4 hover:border-purple-600"
     >
       <input
         type="checkbox"
-        class="w-5 h-5 accent-purple-600"
+        class="h-5 w-5 accent-purple-600"
         .checked=${isSelected}
         @click=${() => {
           if (isSelected) {
@@ -284,7 +275,7 @@ const addonHtml = (addon: Addon) => {
       />
       <div class="ml-4 flex-grow">
         <p class="font-semibold text-blue-950">${addon.name}</p>
-        <p class="text-gray-500 text-sm">${addon.description}</p>
+        <p class="text-sm text-gray-500">${addon.description}</p>
       </div>
       <p class="text-purple-600">
         +&dollar;${addon[state.schedule]}/${moOrYr()}
@@ -295,38 +286,40 @@ const addonHtml = (addon: Addon) => {
 
 const step3CardHtml = () =>
   html`<div class="flex flex-col">
-    <h1 class="text-3xl mx-4 mt-4 font-bold text-blue-950">Pick add-ons</h1>
+    <h1 class="mx-4 mt-4 text-3xl font-bold text-blue-950">Pick add-ons</h1>
     <p class="mx-4 mt-4 text-gray-500">
       Add-ons help enhance your gaming experience.
     </p>
-    <div class="flex flex-col mx-4">${addons.map(addonHtml)}</div>
+    <div class="mx-4 flex flex-col">${addons.map(addonHtml)}</div>
   </div>`;
 
 const step4CardHtml = () =>
   html`<div class="flex flex-col">
-    <h1 class="text-3xl mx-4 mt-4 font-bold text-blue-950">Finishing up</h1>
+    <h1 class="mx-4 mt-4 text-3xl font-bold text-blue-950">Finishing up</h1>
     <p class="mx-4 mt-4 text-gray-500">
       Double-check everything looks OK before confirming.
     </p>
-    <div class="flex flex-col m-4">
-      <div class="bg-blue-100 rounded-t-lg p-4 flex flex-row justify-between">
+    <div class="m-4 flex flex-col">
+      <div class="flex flex-row justify-between rounded-t-lg bg-blue-100 p-4">
         <div class="flex flex-col">
           <p class="font-semibold text-blue-950">
             ${state.plan.name} (${monthlyBilling() ? "Monthly" : "Yearly"})
           </p>
-          <a class="text-gray-500">Change</a>
+          <a class="text-gray-500 underline" @click=${() => (state.step = 2)}>
+            Change
+          </a>
         </div>
-        <span class="text-blue-950"
-          >&dollar;${state.plan[state.schedule]}/${moOrYr()}</span
-        >
+        <span class="text-blue-950">
+          &dollar;${state.plan[state.schedule]}/${moOrYr()}
+        </span>
       </div>
-      <div class="bg-blue-100 rounded-b-lg p-4 flex flex-row justify-between">
-        <div class="flex flex-col text-gray-500 w-1/1">
+      <div class="flex flex-row justify-between rounded-b-lg bg-blue-100 p-4">
+        <div class="flex w-1/1 flex-col text-gray-500">
           ${addons
             .filter(addon => state.addons.has(addon))
             .map(
               addon =>
-                html` <div class="flex flex-row justify-between w-1/1">
+                html` <div class="flex w-1/1 flex-row justify-between">
                   <div>${addon.name}</div>
                   <div class="text-blue-950">
                     +&dollar;${addon[state.schedule]}/${moOrYr()}
@@ -335,11 +328,11 @@ const step4CardHtml = () =>
             )}
         </div>
       </div>
-      <div class="p-4 flex flex-row justify-between w-1/1">
+      <div class="flex w-1/1 flex-row justify-between p-4">
         <div class="text-gray-500">
           Total (per ${monthlyBilling() ? "month" : "year"})
         </div>
-        <div class="text-purple-600 font-semibold">
+        <div class="font-semibold text-purple-600">
           +&dollar;${total()}/${moOrYr()}
         </div>
       </div>
@@ -347,11 +340,11 @@ const step4CardHtml = () =>
   </div>`;
 
 const step5CardHtml = () =>
-  html`<div class="flex flex-col items-center h-[40vh] md:h-1/1">
+  html`<div class="flex h-[40vh] flex-col items-center md:h-1/1">
     <div class="grow-1"></div>
     ${thankYouIcon}
-    <h1 class="text-3xl mx-4 mt-4 font-bold text-blue-950">Thank you!</h1>
-    <p class="mx-4 mt-4 text-gray-500 text-center md:w-md">
+    <h1 class="mx-4 mt-4 text-3xl font-bold text-blue-950">Thank you!</h1>
+    <p class="mx-4 mt-4 text-center text-gray-500 md:w-md">
       Thanks for confirming your subscription! We hope you have fun using our
       platform. If you ever need support, please feel free to email us at
       support@loremgaming.com.
@@ -359,8 +352,31 @@ const step5CardHtml = () =>
     <div class="grow-1"></div>
   </div>`;
 
+const stepGuideHtml = () =>
+  html`<section
+    class="bg-[url(../assets/images/bg-sidebar-mobile.svg)] bg-cover md:rounded-lg md:bg-[url(../assets/images/bg-sidebar-desktop.svg)] md:bg-no-repeat"
+  >
+    <div class="m-auto flex flex-row justify-center pt-10 pb-20 md:flex-col">
+      ${[1, 2, 3, 4].map(
+        n =>
+          html` <div class="justify-left m-4 flex items-center gap-4">
+            <span
+              class="${n === state.step || (n === 4 && state.step > 4)
+                ? "bg-blue-200 text-black"
+                : "bg-transparent text-blue-200"} rounded-full border border-blue-200 px-4 py-2 text-lg font-bold"
+            >
+              ${n}
+            </span>
+            <span class="hidden md:block">
+              <p class="text-grey-500">Step ${n}</p>
+              <p class="font-bold text-purple-200">${stepTags[n - 1]}</p>
+            </span>
+          </div>`
+      )}
+    </div>
+  </section>`;
+
 const stepRenderers = [
-  null,
   step1CardHtml,
   step2CardHtml,
   step3CardHtml,
@@ -371,55 +387,27 @@ const stepRenderers = [
 const pageHtml = () =>
   html` <main class="min-h-screen bg-blue-100 flex flex-col justify-start md:justify-center">
   <div class="flex flex-col md:flex-row md:m-auto md:bg-white md:rounded-lg md:p-4 md:min-w-3/5">
-    <section
-      class="bg-cover md:bg-no-repeat bg-[url(../assets/images/bg-sidebar-mobile.svg)] md:bg-[url(../assets/images/bg-sidebar-desktop.svg)] md:rounded-lg"
-    >
-      <div class="m-auto pt-10 pb-20 flex flex-row md:flex-col justify-center">
-        ${[1, 2, 3, 4].map(
-          n =>
-            html` <div class="m-4 flex justify-left items-center gap-4">
-              <span
-                class="text-lg font-bold rounded-full border border-blue-200 px-4 py-2${classMap(
-                  {
-                    "bg-transparent": n !== state.step && state.step < 5,
-                    "bg-blue-200":
-                      n === state.step || (n === 4 && state.step > 4),
-                    "text-black":
-                      n === state.step || (n === 4 && state.step > 4),
-                    "text-blue-200": n !== state.step && state.step < 5,
-                  }
-                )}"
-              >
-                ${n}
-              </span>
-              <span class="hidden md:block">
-                <p class="text-grey-500">Step ${n}</p>
-                <p class="text-purple-200 font-bold">${stepTags[n - 1]}</p>
-              </span>
-            </div>`
-        )}
-      </div>
-    </section>
+    ${stepGuideHtml()}
     <div class="md:grow-1"></div>
     <section class="flex flex-col mx-4 mt-[-40px] md:mt-0 bg-white rounded-lg md:grow-2">
-      ${stepRenderers[state.step]!()}
+      ${stepRenderers[state.step - 1]!()}
       <div class="mb-4"></div>
       <div class="flex-grow"></div>
       ${
         state.step > 4
           ? null
-          : html`<section class="bg-white p-4 flex justify-end">
+          : html`<section class="flex justify-end bg-white p-4">
               ${state.step === 1
                 ? null
                 : html`<button
-                      class="px-4 py-2 font-semibold"
+                      class="px-4 py-2 text-gray-500"
                       @click=${() => state.step--}
                     >
                       Go Back
                     </button>
                     <div class="flex-grow"></div>`}
               <button
-                class="bg-blue-950 text-white px-4 py-2 rounded-md"
+                class="rounded-md bg-blue-950 px-4 py-2 text-white"
                 @click=${() => {
                   state.validate_step = state.step;
                   if (mayAdvanceStep()) {
@@ -434,56 +422,6 @@ const pageHtml = () =>
     </section>
     <div class="md:grow-1"></div>
   </main>`;
-
-const page = html`
-  <!-- Sidebar start -->
-
-  Step 1 Your info Step 2 Select plan Step 3 Add-ons Step 4 Summary
-
-  <!-- Sidebar end -->
-
-  <!-- Step 1 start -->
-
-  Personal info Please provide your name, email address, and phone number. Name
-  e.g. Stephen King Email Address e.g. stephenking@lorem.com Phone Number e.g.
-  +1 234 567 890 Next Step
-
-  <!-- Step 1 end -->
-
-  <!-- Step 2 start -->
-
-  Select your plan You have the option of monthly or yearly billing. Arcade
-  $9/mo Advanced $12/mo Pro $15/mo Monthly Yearly Go Back Next Step
-
-  <!-- Step 2 end -->
-
-  <!-- Step 3 start -->
-
-  Pick add-ons Add-ons help enhance your gaming experience. Online service
-  Access to multiplayer games +$1/mo Larger storage Extra 1TB of cloud save
-  +$2/mo Customizable Profile Custom theme on your profile +$2/mo Go Back Next
-  Step
-
-  <!-- Step 3 end -->
-
-  <!-- Step 4 start -->
-
-  Finishing up Double-check everything looks OK before confirming.
-
-  <!-- Dynamically add subscription and add-on selections here -->
-
-  Total (per month/year) Go Back Confirm
-
-  <!-- Step 4 end -->
-
-  <!-- Step 5 start -->
-
-  Thank you! Thanks for confirming your subscription! We hope you have fun using
-  our platform. If you ever need support, please feel free to email us at
-  support@loremgaming.com.
-
-  <!-- Step 5 end -->
-`;
 
 const renderBody = () => render(pageHtml(), document.body);
 window.onchange = window.onclick = renderBody;
